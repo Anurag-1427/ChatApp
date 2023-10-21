7eb18743807fd3af1ca586614908bd6cce6b64e2
Anurag-1427 <kaushikanurag2001@gmail.com>
2023-10-21 20:44:43 +0530

    Application initialized
    DESCRIPTION: backend and frontend initialized with chakra ui (for css)

---
+++ backend/dataDummy/data.js
@@ -0,0 +1,94 @@
+const chats = [
+  {
+    isGroupedChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Anurag",
+        email: "anurag@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupedChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Anurag",
+        email: "anurag@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupedChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Anurag",
+        email: "anurag@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupedChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Anurag",
+        email: "anurag@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupedChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Anurag",
+        email: "anurag@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupedChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Anurag",
+        email: "anurag@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+];
+
+module.exports = { chats };
