27448c1cbae1031892068fdd888294936dbd2040
Anurag-1427 <kaushikanurag2001@gmail.com>
2023-12-09 09:52:24 +0530

    feat: add router, db model in backend
    DESCRIPTION:1. Add react router for navigation
    2. Design DB Schema
    3. Add axios
    4. Connect frontend to backend by adding proxy

--- backend/dataDummy/data.js
+++ backend/dataDummy/data.js
@@ -1,93 +1,109 @@
 const chats = [
   {
-    isGroupedChat: false,
+    isGroupChat: false,
     users: [
       {
         name: "John Doe",
         email: "john@example.com",
       },
       {
-        name: "Anurag",
-        email: "anurag@example.com",
+        name: "Piyush",
+        email: "piyush@example.com",
       },
     ],
     _id: "617a077e18c25468bc7c4dd4",
     chatName: "John Doe",
   },
   {
-    isGroupedChat: false,
+    isGroupChat: false,
     users: [
       {
-        name: "John Doe",
-        email: "john@example.com",
+        name: "Guest User",
+        email: "guest@example.com",
       },
       {
-        name: "Anurag",
-        email: "anurag@example.com",
+        name: "Piyush",
+        email: "piyush@example.com",
       },
     ],
-    _id: "617a077e18c25468bc7c4dd4",
-    chatName: "John Doe",
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
   },
   {
-    isGroupedChat: false,
+    isGroupChat: false,
     users: [
       {
-        name: "John Doe",
-        email: "john@example.com",
+        name: "Anthony",
+        email: "anthony@example.com",
       },
       {
-        name: "Anurag",
-        email: "anurag@example.com",
+        name: "Piyush",
+        email: "piyush@example.com",
       },
     ],
-    _id: "617a077e18c25468bc7c4dd4",
-    chatName: "John Doe",
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anthony",
   },
   {
-    isGroupedChat: false,
+    isGroupChat: true,
     users: [
       {
         name: "John Doe",
-        email: "john@example.com",
+        email: "jon@example.com",
       },
       {
-        name: "Anurag",
-        email: "anurag@example.com",
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
       },
     ],
-    _id: "617a077e18c25468bc7c4dd4",
-    chatName: "John Doe",
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
   },
   {
-    isGroupedChat: false,
+    isGroupChat: false,
     users: [
       {
-        name: "John Doe",
-        email: "john@example.com",
+        name: "Jane Doe",
+        email: "jane@example.com",
       },
       {
-        name: "Anurag",
-        email: "anurag@example.com",
+        name: "Piyush",
+        email: "piyush@example.com",
       },
     ],
-    _id: "617a077e18c25468bc7c4dd4",
-    chatName: "John Doe",
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Jane Doe",
   },
   {
-    isGroupedChat: false,
+    isGroupChat: true,
     users: [
       {
         name: "John Doe",
-        email: "john@example.com",
+        email: "jon@example.com",
       },
       {
-        name: "Anurag",
-        email: "anurag@example.com",
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
       },
     ],
-    _id: "617a077e18c25468bc7c4dd4",
-    chatName: "John Doe",
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
   },
 ];
 
