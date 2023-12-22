49fb0a2275d5fd7d74ec2e25929fa8d5239b7084
Anurag-1427 <kaushikanurag2001@gmail.com>
2023-12-22 16:19:11 +0530

    feat: user auth related routes and controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 // Register route
-router.route("/").post(registerUser).get(allUsers);
+router.route("/").post(registerUser).get(protect, allUsers);
 
 // Login route
 router.post("/login", authUser);
